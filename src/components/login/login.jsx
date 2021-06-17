5d8fb3914bccb4e92c48e4a8bfa910026b7959e1
sean-one <basement365@gmail.com>
2021-06-17 13:24:31 -0700

    creating minimal styling for error display

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import AxiosInstance from '../../helpers/axios';
 import './login.css';
@@ -8,29 +8,48 @@ import UserContext from '../../context/userContext';
 
 const Login = () => {
     const { setUserProfile } = useContext(UserContext);
+    const [ passwordError, setPasswordError ] = useState(false); 
+    const [ userError, setUserError ] = useState(false);
+    const [ netError, setNetError ] = useState(false);
     let history = useHistory();
 
     const sendLogin = (e) => {
         e.preventDefault();
+        setPasswordError(false)
+        setUserError(false)
+        setNetError(false)
         const userDetails = {
             username: e.target.username.value,
             password: e.target.password.value
         }
         AxiosInstance.post('/users/login', userDetails)
             .then(response => {
+                console.log(response)
                 if(response.status === 200) {
+                    setUserProfile(response.data)
                     localStorage.setItem('token', response.data.token);
                     localStorage.setItem('userId', response.data.id)
                     localStorage.setItem('user', JSON.stringify(response.data))
-                    setUserProfile(response.data)
                     localStorage.setItem('isLoggedIn', true)
                     history.push('/profile');
                 } else {
+                    console.log('inside else')
                     throw new Error('invalid stuffs');
                 }
             })
             .catch(err => {
-                console.log(err.name + ': ' + err.message)
+                if(!err.respopnse) {
+                    setNetError(true)
+                    console.log('network error')
+                } else if(err.response.status === 401) {
+                    setPasswordError(true)
+                    console.log('invalid credentials')
+                } else if(err.response.status === 404) {
+                    setUserError(true)
+                    console.log('user not found')
+                } else {
+                    console.log(err.name + ': ' + err.message)
+                }
             })
     }
     return (
@@ -39,10 +58,21 @@ const Login = () => {
             <form className='loginform' onSubmit={sendLogin}>
                 <label htmlFor='username'>Username:</label>
                 <input type='text' id='username' name='username' required/>
+                {userError && <p className='errormessage'>user not found!</p>}
                 <label htmlFor='password'>Password:</label>
-                <input type='password' id='password' name='password' required/>
+                <input type='password' id='password' name='password' required />
+                {netError && <p className='errormessage'>network error, please wait a moment then try again</p>}
+                {passwordError && <p className='errormessage'>wrong password!</p>}
                 <input type='submit' value='submit' />
             </form>
+            <div className='registernew'>
+                <h3>No account yet?</h3>
+                <Link to={{
+                    pathname: '/register'
+                }}>
+                    <button>Register New Account</button>
+                </Link>
+            </div>
         </div>
     )
 }
