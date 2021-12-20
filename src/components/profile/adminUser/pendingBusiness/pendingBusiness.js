03c58fc3ad063969b30fbe50da3d0aba85f1214b
sean-one <basement365@gmail.com>
2021-12-20 09:50:51 -0800

    update pending business request

--- src/components/profile/adminUser/pendingBusiness/pendingBusiness.js
+++ src/components/profile/adminUser/pendingBusiness/pendingBusiness.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useForm } from 'react-hook-form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes, faCaretDown, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 import AxiosInstance from '../../../../helpers/axios';
@@ -6,7 +7,30 @@ import AxiosInstance from '../../../../helpers/axios';
 import { UserAdminContext } from '../../../../context/adminuser/adminuser.provider';
 
 const PendingBusiness = (props) => {
+    const [ refresher, setRefresher ] = useState(false)
     const { pendingBusinessRequest, setPendingBusinessRequestList } = useContext(UserAdminContext)
+    const { register, handleSubmit, reset } = useForm()
+
+    const updateBusinessRequestStatus = (data) => {
+        const businessIds = []
+        // remove null keys and value pair where value is null
+        let nullRemoved = Object.fromEntries(Object.entries(data).filter(([_, v]) => v != null));
+        Object.keys(nullRemoved).forEach(k => {
+            if (nullRemoved[k] === 'approved') {
+                businessIds.push(k)
+            }
+        })
+
+        AxiosInstance.post('/business/update-approval', businessIds)
+            .then(response => {
+                reset()
+                setRefresher(!refresher)
+                // console.log(response)
+            })
+            .catch(err => console.log(err))
+        
+        return
+    }
 
     useEffect(() => {
         AxiosInstance.get('/business/pending-approval')
@@ -14,7 +38,7 @@ const PendingBusiness = (props) => {
                 setPendingBusinessRequestList(businesses.data)
             })
         // eslint-disable-next-line
-    }, []);
+    }, [refresher]);
 
     return (
         <div>
@@ -37,7 +61,7 @@ const PendingBusiness = (props) => {
                         <FontAwesomeIcon id='requestTableReject' className='requestTableIcons' icon={faTimes} size='1x' />
 
                     </div>
-                    <form className='requestTable'>
+                    <form className='requestTable' onSubmit={handleSubmit(updateBusinessRequestStatus)}>
                         {
                             pendingBusinessRequest.map(request => (
                                 <div className='requestTableRow' key={request.id}>
@@ -45,10 +69,10 @@ const PendingBusiness = (props) => {
                                     <p className='requestTableText'>{request.businesstype}</p>
                                     <p className='requestTableText'>{request.username}</p>
                                     <div className='requestTableButtons'>
-                                        <input type='radio' id='approved' value='approved' />
-                                        <input type='radio' id='rejected' value='rejected' />
-                                        {/* <input {...register(`${request.id}`)} type='radio' id='approved' value='approved' />
-                                        <input {...register(`${request.id}`)} type='radio' id='rejected' value='rejected' /> */}
+                                        <input {...register(`${request.id}`)} type='radio' id='approved' value='approved' />
+                                        <input {...register(`${request.id}`)} type='radio' id='rejected' value='rejected' />
+                                        {/* <input type='radio' id='approved' value='approved' /> */}
+                                        {/* <input type='radio' id='rejected' value='rejected' /> */}
                                     </div>
                                 </div>
                             ))
