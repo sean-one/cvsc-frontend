83bacb6f814456ca81562cc2dc4364597f658285
sean-one <basement365@gmail.com>
2023-02-08 18:52:52 -0800

    working on avatar image previews

--- src/components/profile/account/user.account.js
+++ src/components/profile/account/user.account.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { Button, FloatingLabel, Form, Image } from 'react-bootstrap';
@@ -7,7 +7,8 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import useAuth from '../../../hooks/useAuth';
 import useNotification from '../../../hooks/useNotification'
 import { useEventsQuery } from '../../../hooks/useEventsApi';
-import default_profile from '../../../assets/default_user_icon.png'
+// import default_profile from '../../../assets/default_user_icon.png'
+import useAvatarPreview from '../../../hooks/useAvatarPreview';
 import { image_link, role_types } from '../../../helpers/dataCleanUp';
 import AxiosInstance from '../../../helpers/axios';
 import { editUserSchema } from '../../../helpers/validationSchemas';
@@ -18,7 +19,8 @@ const UserAccount = () => {
     const [ editView, setEditView ] = useState(false)
     const { refetch } = useEventsQuery()
     const { auth, logout_user, setAuth } = useAuth()
-    const [ imagePreview, setImagePreview ] = useState(auth?.user?.avatar)
+    const { imagePreview, canvas } = useAvatarPreview(auth?.user?.avatar)
+    // const [ imagePreview, setImagePreview ] = useState(auth?.user?.avatar)
 
     let navigate = useNavigate()
 
@@ -38,13 +40,13 @@ const UserAccount = () => {
     const update_password = watch('update_password', false)
     const update_image = watch('update_image', false)
 
-    const image_preview = (e) => {
-        if(e.target.files.length !== 0){
-            setImagePreview(URL.createObjectURL(e.target.files[0]))
-        } else {
-            setImagePreview(auth?.user?.avatar)
-        }
-    }
+    // const image_preview = (e) => {
+    //     if(e.target.files.length !== 0){
+    //         setImagePreview(URL.createObjectURL(e.target.files[0]))
+    //     } else {
+    //         setImagePreview(auth?.user?.avatar)
+    //     }
+    // }
 
     const update_user = async (data) => {
         try {
@@ -83,7 +85,7 @@ const UserAccount = () => {
                 setAuth({ user: updated_user_response.data.user, roles: updated_user_response.data.roles })
                 
                 setEditView(false)
-                setImagePreview(updated_user_response.data.user?.avatar)
+                // setImagePreview(updated_user_response.data.user?.avatar)
 
                 dispatch({
                     type: "ADD_NOTIFICATION",
@@ -119,7 +121,7 @@ const UserAccount = () => {
     }
 
     const close_edit_view = () => {
-        setImagePreview(auth?.user?.avatar)
+        // setImagePreview(auth?.user?.avatar)
         setEditView(false)
         reset()
     }
@@ -150,7 +152,14 @@ const UserAccount = () => {
         <div className='d-flex flex-column border mb-3'>
             
             <div className='p-5 text-center'>
-                <Image thumbnail roundedCircle src={(imagePreview === null) ? default_profile : image_link(imagePreview)} alt={`user avatar`} />
+                {/* <Image thumbnail roundedCircle src={(imagePreview === null) ? default_profile : image_link(imagePreview)} alt={`user avatar`} /> */}
+                <canvas
+                    className='rounded-circle'
+                    id={'avatarImagePreview'}
+                    ref={canvas}
+                    width={300}
+                    height={300}
+                />
             </div>
             
             <div className='d-flex justify-content-between'>
@@ -183,7 +192,7 @@ const UserAccount = () => {
                                                 type='file'
                                                 name='avatar'
                                                 accept='image/*'
-                                                onChange={(e) => image_preview(e)}
+                                                onChange={(e) => imagePreview(e)}
                                             />
                                             <div className='errormessage'>{errors.profile_image?.message}</div>
                                         </Form.Group>
---
+++ src/hooks/useAvatarPreview.js
@@ -0,0 +1,69 @@
+import { useState, useRef, useEffect } from 'react';
+import default_profile from '../assets/default_user_icon.png';
+
+
+const useAvatarPreview = (user_avatar) => {
+    console.log(user_avatar)
+    console.log(default_profile)
+    const [editImage, setEditImage] = useState(false)
+    const [imageToUpload, setImageToUpload] = useState()
+    const canvas = useRef(null)
+
+    const imagePreview = (event) => {
+        setEditImage(true)
+        let fileToUpload = event.target.files
+        let reader = new FileReader()
+        const previewImage = new Image()
+        reader.onload = function (e) {
+            previewImage.src = e.target.result
+            previewImage.onload = () => setImageToUpload(previewImage)
+        }
+        reader.readAsDataURL(fileToUpload[0])
+    }
+
+    useEffect(() => {
+        let mounted = true
+        if (mounted) {
+            if (imageToUpload && canvas) {
+                const ctx = canvas.current.getContext('2d')
+                const MAX_WIDTH = canvas.current.width
+                const MAX_HEIGHT = canvas.current.height
+                let width = imageToUpload.width
+                let height = imageToUpload.height
+
+
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width
+                    width = MAX_WIDTH
+                } else if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height
+                    height = MAX_HEIGHT
+                } else {
+                    if (width > height) {
+                        width = MAX_WIDTH
+                        height *= width / MAX_WIDTH
+                    } else {
+                        height *= MAX_WIDTH / width
+                        width = MAX_WIDTH
+                    }
+                }
+
+                // crop canvas to the size of the drawing
+                canvas.current.width = width
+                canvas.current.height = height
+
+                ctx.clearRect(0, 0, canvas.current.width, canvas.current.height)
+
+                ctx.drawImage(imageToUpload, (canvas.current.width / 2) - (width / 2), (canvas.current.height / 2) - (height / 2), width, height)
+            }
+        }
+
+        return () => {
+            mounted = false
+        }
+    }, [imageToUpload, canvas])
+
+    return { editImage, imagePreview, imageToUpload, canvas }
+}
+
+export default useAvatarPreview;
