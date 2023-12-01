c1de36f5ccdd70a503159d32aa46b257e05e3dbd
sean-one <basement365@gmail.com>
2023-11-30 17:20:27 -0800

    working on business auth

--- src/components/auth/businessManagementAuth.js
+++ src/components/auth/businessManagementAuth.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useParams } from "react-router";
 import { useUserBusinessRole } from "../../hooks/useRolesApi";
 
@@ -6,6 +7,8 @@ import { useUserBusinessRole } from "../../hooks/useRolesApi";
 const BusinessManagementAuth = ({ children }) => {
     const { business_id } = useParams()
     const { data: user_role, status: role_status, error } = useUserBusinessRole(business_id)
+
+    let navigate = useNavigate()
 
     if (role_status === 'loading') {
         return <div>loading...</div>
@@ -17,6 +20,10 @@ const BusinessManagementAuth = ({ children }) => {
 
     // if no role is found - returns undefined
     const user_business_role = user_role.data
+
+    if (user_business_role?.role_type === process.env.REACT_APP_CREATOR_ACCOUNT) {
+        navigate('/')
+    }
     
     const childWithProps = React.cloneElement(children, { userBusinessRole: user_business_role })
 
