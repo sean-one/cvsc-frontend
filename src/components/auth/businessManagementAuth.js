e5cfa2874a35325a6f4af0e2b9233be1bd3c9b63
sean-one <basement365@gmail.com>
2023-12-12 12:05:33 -0800

    working on useEffect removal from business auth

--- src/components/auth/businessManagementAuth.js
+++ src/components/auth/businessManagementAuth.js
@@ -9,42 +9,26 @@ const BusinessManagementAuth = ({ children }) => {
     const [ isLoadingUser, setIsLoadingUser ] = useState(true)
     const { business_id } = useParams()
     const { dispatch } = useNotification()
-    const { data: user_role, status: role_status } = useUserBusinessRole(business_id)
+    const { data: user_role, status: role_status, error } = useUserBusinessRole(business_id)
 
     let navigate = useNavigate()
-    let user_business_role = user_role?.data
     
-    console.log('user_business_role - init')
-    console.log(user_business_role)
-    console.log(user_role?.data)
-    useEffect(() => {
-        console.log('inside useEffect')
-        console.log(user_business_role)
-        if (!isLoadingUser && (user_business_role?.role_type === process.env.REACT_APP_CREATOR_ACCOUNT || user_business_role === undefined)) {
-            dispatch({
-                type: "ADD_NOTIFICATION",
-                payload: {
-                    notification_type: 'ERROR',
-                    message: 'invalid business role permissions'
-                }
-            });
-
-            navigate('/');
-        }
-    }, [isLoadingUser, user_business_role, dispatch, navigate]);
-    
-    if (isLoadingUser) {
-        return <div>loading...</div>
+    if (role_status === 'loading') {
+        return (
+            <div>loading...</div>
+            )
     }
 
-    if (role_status === 'success') {
-        setIsLoadingUser(false)
+    if (role_status === 'error') {
+        console.log(error)
+        return (
+            <div>there was an error</div>
+        )
     }
-    console.log('user_business_role - after role_status success')
-    console.log(user_business_role)
-    console.log(user_role.data)
 
+    const user_business_role = user_role?.data
     const childWithProps = React.cloneElement(children, { userBusinessRole: user_business_role })
+
 
     return (
         <div>
