df6545bfc10c3ad2e33a6651390c6f74d39c6e9c
sean-one <basement365@gmail.com>
2023-12-12 20:00:42 -0800

    remove business management auth comments

--- src/components/auth/businessManagementAuth.js
+++ src/components/auth/businessManagementAuth.js
@@ -1,30 +1,16 @@
 import React from 'react';
-// import React, { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
 import { useParams } from "react-router";
 
-// import useNotification from '../../hooks/useNotification';
 import { useManagementRole } from "../../hooks/useRolesApi";
 
 const BusinessManagementAuth = ({ children }) => {
-    // const [ isLoadingUser, setIsLoadingUser ] = useState(true)
     const { business_id } = useParams()
-    // const { dispatch } = useNotification()
-    const { data: user_role, status: role_status, error } = useManagementRole(business_id)
+    const { data: user_role, status: role_status } = useManagementRole(business_id)
 
-    // let navigate = useNavigate()
-    
     if (role_status === 'loading') {
         return (
             <div>loading...</div>
             )
-    }
-
-    if (role_status === 'error') {
-        // 401 token missing or expired
-        // 404 no role found - forward to profile view
-        console.log(error)
-        console.log(error.response)
     }
 
     const user_business_role = user_role?.data
