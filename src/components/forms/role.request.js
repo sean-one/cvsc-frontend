f96579be53c3ff25dbf7bf28838c9aa47c455c06
sean-one <basement365@gmail.com>
2024-02-22 18:56:32 -0800

    added content for when no businesses are showing in role request

--- src/components/forms/role.request.js
+++ src/components/forms/role.request.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 import useNotification from '../../hooks/useNotification';
@@ -7,12 +9,25 @@ import { useCreateRoleMutation } from '../../hooks/useRolesApi';
 import { useBusinessesQuery } from '../../hooks/useBusinessApi';
 import { uuidPattern } from './utils/form.validations';
 
+const RoleRequestStyles = styled.div`
+    .noShowRoleRequest {
+        text-align: center;
+
+        div {
+            margin-bottom: 1.5rem;
+        }
+    }
+`;
+
+
 const RoleRequest = ({ user_roles }) => {
     const businessIdList = user_roles.map(role => role?.business_id) || []
     const { dispatch } = useNotification();
 
     const { data: businesses_list, isPending, isError, error: businesses_list_error } = useBusinessesQuery()
     const { mutate: createRole } = useCreateRoleMutation()
+
+    let navigate = useNavigate();
 
     const { register, handleSubmit, reset, clearErrors, watch, formState:{ errors } } = useForm({
         mode: 'onBlur',
@@ -56,32 +71,38 @@ const RoleRequest = ({ user_roles }) => {
 
 
     return (
-        <div>
-            {
-                (business_filtered.length > 0) &&
-                    <div className='sectionContainer'>
-                        <div className='subheaderText'>Create Business Role Request</div>
+        <RoleRequestStyles>
+            <div>
+                {
+                    (business_filtered.length > 0)
+                        ? <div className='sectionContainer'>
+                            <div className='subheaderText'>Create Business Role Request</div>
 
-                        <form onSubmit={handleSubmit(roleCreate)} className='standardForm'>
-                            <select {...register('business_id', {
-                                required: 'valid business is required',
-                                pattern: uuidPattern
-                            })} type='text' onClick={() => clearErrors('business_id')}>
-                                <option value=''>Select a business...</option>
-                                {
-                                    business_filtered.map(business => (
-                                        <option key={business.id} value={business.id}>
-                                            {business.business_name.toUpperCase()}
-                                        </option>
-                                    ))
-                                }
-                            </select>
-                            {errors.business_id ? <div className='errormessage'>{errors.business_id?.message}</div> : null}
-                            <button disabled={!selectedBusinessId} type='submit'>Submit Role Request</button>
-                        </form>
-                    </div>
-            }
-        </div>
+                            <form onSubmit={handleSubmit(roleCreate)} className='standardForm'>
+                                <select {...register('business_id', {
+                                    required: 'valid business is required',
+                                    pattern: uuidPattern
+                                })} type='text' onClick={() => clearErrors('business_id')}>
+                                    <option value=''>Select a business...</option>
+                                    {
+                                        business_filtered.map(business => (
+                                            <option key={business.id} value={business.id}>
+                                                {business.business_name.toUpperCase()}
+                                            </option>
+                                        ))
+                                    }
+                                </select>
+                                {errors.business_id ? <div className='errormessage'>{errors.business_id?.message}</div> : null}
+                                <button disabled={!selectedBusinessId} type='submit'>Submit Role Request</button>
+                            </form>
+                        </div>
+                        : <div className='noShowRoleRequest'>
+                            <div>Role request will be shown when businesses are available.</div>
+                            <div onClick={() => navigate('/business/create')}>click here to create a new business</div>
+                        </div>
+                }
+            </div>
+        </RoleRequestStyles>
     )
 }
 
