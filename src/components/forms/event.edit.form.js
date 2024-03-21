a67a0281121535427a888767e66c65c669071221
sean-one <basement365@gmail.com>
2024-03-21 13:01:26 -0700

    added new select

--- src/components/forms/event.edit.form.js
+++ src/components/forms/event.edit.form.js
@@ -16,6 +16,7 @@ import { image_link } from '../../helpers/dataCleanUp';
 import { AddImageIcon, DateIcon, TimeIcon } from '../icons/siteIcons';
 import { validateEventDate, validateEventTime, validateNONEmptyString } from './utils/form.validations';
 import AddressForm from './address.form';
+import LoadingSpinner from '../loadingSpinner';
 
 
 const EditEventFormStyles = styled.div`
@@ -104,7 +105,7 @@ const EventEditForm = () => {
     const { editImage, imagePreview, canvas, setEditImage } = useEventImagePreview()
     const { dispatch } = useNotification()
     
-    const { data: user_roles, isSuccess: userRoleSuccess } = useUserRolesQuery(auth?.user?.id);
+    const { data: user_roles, isError, isPending, isSuccess: userRoleSuccess } = useUserRolesQuery(auth?.user?.id);
     
     const { mutateAsync: updateEventMutation } = useUpdateEventMutation()
     const { mutate: removeEventMutation } = useRemoveEventMutation()
@@ -132,6 +133,18 @@ const EventEditForm = () => {
         localStorage.setItem('editEventForm', JSON.stringify(event_data))
         try {
             const formData = new FormData()
+
+            if (event_data.hasOwnProperty('host_business')) {
+                if (event_data.host_business && event_data.host_business.hasOwnProperty('value')) {
+                    event_data.host_business = event_data.host_business.value
+                }
+            }
+
+            if (event_data.hasOwnProperty('business_tag')) {
+                if (event_data.business_tag && event_data.business_tag.hasOwnProperty('value')) {
+                    event_data.business_tag = event_data.business_tag.value
+                }
+            }
 
             // if eventmedia has a file set in formData, for some reason it does not show in dirtyFields
             if(canvas.current !== null) {
@@ -225,137 +238,140 @@ const EventEditForm = () => {
     const hostBusinessDefault = user_host_business_list.find(business => business.value === event_data.host_business)
 
 
-    console.log(hostBusinessDefault)
     return (
         <EditEventFormStyles>
-            <div>
-                <form onSubmit={handleSubmit(update_event)} encType='multipart/form-data' className='standardForm'>
+            {
+                isPending ? (
+                    <LoadingSpinner />
+                ) : isError ? (
+                    null
+                ) : <div>
+                    <form onSubmit={handleSubmit(update_event)} encType='multipart/form-data' className='standardForm'>
 
-                    {/* EVENT NAME AND MEDIA IMAGE */}
-                    <div className='formRowInputIcon'>
+                        {/* EVENT NAME AND MEDIA IMAGE */}
+                        <div className='formRowInputIcon'>
 
-                        {/* EVENT NAME */}
-                        <div className='inputWrapper'>
-                            <input {...register('eventname', {
-                                minLength: {
-                                    value: 4,
-                                    message: 'an event name must have at least 4 characters'
-                                },
-                                maxLength: {
-                                    value: 49,
-                                    message: 'an event name must have less then 50 characters'
+                            {/* EVENT NAME */}
+                            <div className='inputWrapper'>
+                                <input {...register('eventname', {
+                                    minLength: {
+                                        value: 4,
+                                        message: 'an event name must have at least 4 characters'
+                                    },
+                                    maxLength: {
+                                        value: 49,
+                                        message: 'an event name must have less then 50 characters'
+                                    }
+                                })} type='text' onClick={() => clearErrors('eventname')} />
+                            </div>
+
+                            {/* EVENT MEDIA UPDATE */}
+                            <label htmlFor='eventmedia' className='inputLabel' onClick={() => clearErrors('eventmedia')}>
+                                <AddImageIcon />
+                                <input {...register('eventmedia')} id='eventmedia' className='inputLabelInput' type='file' accept='image/*' onChange={(e) => imagePreview(e)} />
+                            </label>
+
+                        </div>
+                        {errors.eventname ? <div className='errormessage'>{errors.eventname?.message}</div> : null}
+                        {errors.eventmedia ? <div className='errormessage imageError'>{errors.eventmedia?.message}</div>: null}
+
+                        {
+                            editImage
+                                ? <div className='formImage'>
+                                    <canvas
+                                        id={'avatarImagePreview'}
+                                        ref={canvas}
+                                    />
+                                </div>
+                                : <div className='formImage'>
+                                    <img
+                                        src={image_link(event_data?.eventmedia)}
+                                        alt={event_data?.eventname}
+                                    />
+                                </div>
+                        }
+
+                        <AddressForm
+                            register={register}
+                            setValue={setValue}
+                            errors={errors}
+                            clearErrors={clearErrors}
+                            currentValue={event_data?.formatted_address}
+                        />
+
+                        {/* EVENT DATE */}
+                        <div className='dateTimeInputWrapper'>
+                            <label htmlFor='eventdate'><DateIcon /></label>
+                            <input {...register('eventdate', {
+                                validate: {
+                                    checkEmptyString: validateNONEmptyString,
+                                    validateDateFormat: (value) => validateEventDate(value, false)
                                 }
-                            })} type='text' onClick={() => clearErrors('eventname')} />
+                            })} type='date' onClick={() => clearErrors('eventdate')} />
+                        </div>
+                        {errors.eventdate ? <div className='errormessage'>{errors.eventdate?.message}</div> : null}
+
+                        {/* EVENT START TIME */}
+                        <div className='dateTimeInputWrapper'>
+                            <label htmlFor='eventstart'><TimeIcon /></label>
+                            <input {...register('eventstart', {
+                                validate: {
+                                    checkEmptyString: validateNONEmptyString,
+                                    validateTimeFormat: (value) => validateEventTime(value, false)
+                                }
+                            })} type='time' onClick={() => clearErrors('eventstart')} />
+                        </div>
+                        {errors.eventstart ? <div className='errormessage'>{errors.eventstart?.message}</div> : null}
+
+                        {/* EVENT END TIME */}
+                        <div className='dateTimeInputWrapper'>
+                            <label htmlFor='eventend'><TimeIcon /></label>
+                            <input {...register('eventend', {
+                                validate: {
+                                    checkEmptyString: validateNONEmptyString,
+                                    validateTimeFormat: (value) => validateEventTime(value, false)
+                                }
+                            })} type='time' onClick={() => clearErrors('eventend')} />
+                        </div>
+                        {errors.eventend ? <div className='errormessage'>{errors.eventend?.message}</div> : null}
+
+                        {/* BUSINESS NAME */}
+                        <div className='inputWrapper'>
+                            <label htmlFor='host_business' className='visuallyHidden'>Business Name:</label>
+                            <Controller
+                                name='host_business'
+                                control={control}
+                                render={({ field }) => (
+                                    <Select
+                                        {...field}
+                                        options={user_host_business_list}
+                                        defaultValue={hostBusinessDefault}
+                                        placeholder='Select a business'
+                                        isClearable
+                                        isSearchable
+                                        styles={customSelectStyles}
+                                        onChange={(selectedOption) => field.onChange(selectedOption)}
+                                    />
+                                )}
+                            />
+                            {errors.host_business && <div className='errormessage'>{errors.host_business.message}</div>}
                         </div>
 
-                        {/* EVENT MEDIA UPDATE */}
-                        <label htmlFor='eventmedia' className='inputLabel' onClick={() => clearErrors('eventmedia')}>
-                            <AddImageIcon />
-                            <input {...register('eventmedia')} id='eventmedia' className='inputLabelInput' type='file' accept='image/*' onChange={(e) => imagePreview(e)} />
-                        </label>
+                        {/* EVENT DETAILS */}
+                        <div className='inputWrapper'>
+                            <textarea {...register('details')} rows='8' onClick={() => clearErrors('details')} />
+                            {errors.details ? <div className='errormessage'>{errors.details?.message}</div> : null}
+                        </div>
+                        
+                        <div className='formButtonWrapper'>
+                            <button type='submit' disabled={(!isDirty || Object.keys(dirtyFields).length === 0) && (canvas.current === null)}>Update</button>
+                            <button type='button' onClick={sendEventDelete}>Delete</button>
+                            <button type='button' onClick={handleClose}>Close</button>
+                        </div>
 
-                    </div>
-                    {errors.eventname ? <div className='errormessage'>{errors.eventname?.message}</div> : null}
-                    {errors.eventmedia ? <div className='errormessage imageError'>{errors.eventmedia?.message}</div>: null}
-
-                    {
-                        editImage
-                            ? <div className='formImage'>
-                                <canvas
-                                    id={'avatarImagePreview'}
-                                    ref={canvas}
-                                />
-                            </div>
-                            : <div className='formImage'>
-                                <img
-                                    src={image_link(event_data?.eventmedia)}
-                                    alt={event_data?.eventname}
-                                />
-                            </div>
-                    }
-
-                    <AddressForm
-                        register={register}
-                        setValue={setValue}
-                        errors={errors}
-                        clearErrors={clearErrors}
-                        currentValue={event_data?.formatted_address}
-                    />
-
-                    {/* EVENT DATE */}
-                    <div className='dateTimeInputWrapper'>
-                        <label htmlFor='eventdate'><DateIcon /></label>
-                        <input {...register('eventdate', {
-                            validate: {
-                                checkEmptyString: validateNONEmptyString,
-                                validateDateFormat: (value) => validateEventDate(value, false)
-                            }
-                        })} type='date' onClick={() => clearErrors('eventdate')} />
-                    </div>
-                    {errors.eventdate ? <div className='errormessage'>{errors.eventdate?.message}</div> : null}
-
-                    {/* EVENT START TIME */}
-                    <div className='dateTimeInputWrapper'>
-                        <label htmlFor='eventstart'><TimeIcon /></label>
-                        <input {...register('eventstart', {
-                            validate: {
-                                checkEmptyString: validateNONEmptyString,
-                                validateTimeFormat: (value) => validateEventTime(value, false)
-                            }
-                        })} type='time' onClick={() => clearErrors('eventstart')} />
-                    </div>
-                    {errors.eventstart ? <div className='errormessage'>{errors.eventstart?.message}</div> : null}
-
-                    {/* EVENT END TIME */}
-                    <div className='dateTimeInputWrapper'>
-                        <label htmlFor='eventend'><TimeIcon /></label>
-                        <input {...register('eventend', {
-                            validate: {
-                                checkEmptyString: validateNONEmptyString,
-                                validateTimeFormat: (value) => validateEventTime(value, false)
-                            }
-                        })} type='time' onClick={() => clearErrors('eventend')} />
-                    </div>
-                    {errors.eventend ? <div className='errormessage'>{errors.eventend?.message}</div> : null}
-
-                    {/* BUSINESS NAME */}
-                    <div className='inputWrapper'>
-                        <label htmlFor='host_business' className='visuallyHidden'>Business Name:</label>
-                        <Controller
-                            name='host_business'
-                            control={control}
-                            rules={{ required: 'a business name is required' }}
-                            render={({ field }) => (
-                                <Select
-                                    {...field}
-                                    // value={hostBusinessDefault}
-                                    options={user_host_business_list}
-                                    defaultInputValue={hostBusinessDefault.label}
-                                    placeholder='Select a business'
-                                    isClearable
-                                    isSearchable
-                                    styles={customSelectStyles}
-                                    onChange={(selectedOption) => field.onChange(selectedOption)}
-                                />
-                            )}
-                        />
-                        {errors.host_business && <div className='errormessage'>{errors.host_business.message}</div>}
-                    </div>
-
-                    {/* EVENT DETAILS */}
-                    <div className='inputWrapper'>
-                        <textarea {...register('details')} rows='8' onClick={() => clearErrors('details')} />
-                        {errors.details ? <div className='errormessage'>{errors.details?.message}</div> : null}
-                    </div>
-                    
-                    <div className='formButtonWrapper'>
-                        <button type='submit' disabled={(!isDirty || Object.keys(dirtyFields).length === 0) && (canvas.current === null)}>Update</button>
-                        <button type='button' onClick={sendEventDelete}>Delete</button>
-                        <button type='button' onClick={handleClose}>Close</button>
-                    </div>
-
-                </form>
-            </div>
+                    </form>
+                </div>
+            }
         </EditEventFormStyles>
     )
 }
