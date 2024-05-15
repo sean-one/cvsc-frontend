78d2d2b8f3dd2d78bfe6465bd16f2f09c640d0f1
sean-one <basement365@gmail.com>
2024-05-15 16:31:00 -0700

    working on mod user page

--- src/components/auth/mod/mod_users.js
+++ src/components/auth/mod/mod_users.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { MdVerified, MdOutlineVerified } from 'react-icons/md';
+import { FaTrashCan } from 'react-icons/fa6';
 
+import useAuth from '../../../hooks/useAuth';
 import useNotification from '../../../hooks/useNotification';
 import AxiosInstance from '../../../helpers/axios';
 import squirrel_master from '../../../assets/squirrel-master.webp';
@@ -10,7 +12,7 @@ import LoadingSpinner from '../../loadingSpinner';
 
 const ModUsersStyles = styled.div`
     .modUsersWrapper {
-        padding: 2rem 1rem;
+        padding: 2rem 0.5rem;
         margin: 0 auto;
         max-width: var(--max-section-width);
         /* background: var(--opacity); */
@@ -38,16 +40,42 @@ const ModUsersStyles = styled.div`
     }
 
     .singleUser {
-        height: 3rem;
+        padding: 1.25rem 0;
+        margin: 0.3rem 0;
         display: flex;
         justify-content: space-between;
         align-items: center;
         gap: 0.5rem;
+        flex-wrap: wrap;
+        border-top: 0.05rem dotted var(--main-highlight-color);
+        border-bottom: 0.05rem dotted var(--main-highlight-color);
+    }
+
+    .singleUserSection {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+    }
+    
+    .singleUserLeft {
+        justify-self: flex-start
+        font-size: 1.7rem;
+    }
+    .singleUserRight {
+        justify-self: flex-end
+        font-size: 1.4rem;
+    }
+    .modUsersIcon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 `;
 
 const ModUsers = () => {
+    const { auth } = useAuth();
     const [ users, setUsers ] = useState([]);
+    const [ searchTerm, setSearchTerm ] = useState('')
     const [ loading, setLoading ] = useState(true);
     const { dispatch } = useNotification()
 
@@ -57,7 +85,8 @@ const ModUsers = () => {
         const getUsers = async () => {
             try {
                 const response = await AxiosInstance.get('/users');
-                setUsers(response.data);
+                const filterMeOut = response.data.filter(user => user.id !== auth?.user?.id)
+                setUsers(filterMeOut);
             } catch (error) {
                 console.error('Error geting users: ', error);
                 dispatch({
@@ -78,13 +107,45 @@ const ModUsers = () => {
         }
 
         getUsers();
-    }, [dispatch, navigate])
+    }, [auth.user, dispatch, navigate])
+
+    const searchQueryUsers = users.filter(user =>
+        user.username.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+    ) || [];
+
+    const banUser = async (user_id) => {
+        try {
+            const response = await AxiosInstance.delete(`/users/squirrel-user-ban/${user_id}`)
+            if (response.status === 204) {
+                const update_user_list = users.filter(user => user.id !== user_id)
+                setUsers(update_user_list)
+
+                dispatch({
+                    type: "ADD_NOTIFICATION",
+                    payload: {
+                        notification_type: 'SUCCESS',
+                        message: 'user account has been deleted'
+                    }
+                })
+            }
+        } catch (error) {
+            console.error('Error setting user ban: ', error)
+            dispatch({
+                type: "ADD_NOTIFICATION",
+                payload: {
+                    notification_type: 'ERROR',
+                    message: 'there was an error banning user'
+                }
+            })
+        }
+    }
 
     if (loading) {
         return <LoadingSpinner />
     }
 
-    console.log(users)
+
     return (
         <ModUsersStyles>
             <div className='modUsersWrapper'>
@@ -92,15 +153,29 @@ const ModUsers = () => {
                     <div className='subheaderText'>Users Mod Section</div>
                     <img onClick={() => navigate('/squirrelmaster')} src={squirrel_master} alt='squirrel' />
                 </div>
+                <div>
+                    <input
+                        type='text'
+                        placeholder='search username or email'
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                    />
+                </div>
                 {
                     (users.length !== 0)
                         ? <div className='modUsersTable'>
                             {
-                                users.map(user => {
+                                searchQueryUsers.map(user => {
                                     return (
                                         <div key={user.id} className='singleUser'>
-                                            <div>{user.username}</div>
-                                            <div>{user.email}{user.email_verified ? <MdVerified /> : <MdOutlineVerified />}</div>
+                                            <div className='singleUserSection singleUserLeft'>
+                                                <div onClick={() => banUser(user.id)} className='smallSiteIcons modUsersIcon' style={{ color: 'var(--error-color)' }}><FaTrashCan /></div>
+                                                <div>{user.username}</div>
+                                            </div>
+                                            <div className='singleUserSection singleUserRight'>
+                                                <div>{user.email}</div>
+                                                <div className='smallSiteIcons modUsersIcon'>{user.email_verified ? <MdVerified /> : <MdOutlineVerified />}</div>
+                                            </div>
                                         </div>
                                     )
                                 })
