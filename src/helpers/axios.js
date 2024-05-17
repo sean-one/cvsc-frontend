7d21aaffce5017c5b36065c3d5bc109e2c59f3c1
sean-one <basement365@gmail.com>
2024-05-17 04:35:25 -0700

    updated base url for axios instance

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,6 +1,9 @@
 import Axios from 'axios';
 
+const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
+
 const AxiosInstance = Axios.create({
+    baseURL: baseURL,
     withCredentials: true,
     method: [ 'GET', 'POST', 'OPTIONS', 'PUT', 'DELETE' ],
 });
